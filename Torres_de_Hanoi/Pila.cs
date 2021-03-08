f2b019c2f377fe8e44c6088d02b76277500708e0
Pablo Enguix <pabloenguixllopis@gmail.com>
2021-03-08 16:11:10 +0100

    Implementación de la clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,35 +9,38 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
         public List<Disco> Elementos { get; set; }
-        */
 
-        /* TODO: Implementar métodos */
+        //Inicializa las variables
         public Pila()
         {
-
+            Size = 0;
+            Top = 0;
+            Elementos = new List<Disco>();
         }
 
+        //Aumenta la cantidad de discos en la pila y el disco que está arriba
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
+            Size++;
+            Top++;
         }
-
+        // Reduce la cantidad de discos en la pila, recarga el disco que está arriba y devuelve el disco que ha quitado
         public Disco pop()
         {
-            return null;
-        }                
-
-        public bool isEmpty()
-        {
-            return true;
+            Disco discoFuera = Elementos.ElementAt(Size - 1);
+            Elementos.RemoveAt(Size - 1);
+            Size--;
+            Top--;
+            return discoFuera;
         }
 
+        // Devuelve true si el tamaño es 0
+        public bool isEmpty()
+        {
+            return Size == 0;
+        }
     }
 }
