6a9f0e8313f360957517641d3c39cf4d6bd13a1e
Pablo Enguix <pabloenguixllopis@gmail.com>
2021-03-08 16:33:01 +0100

    Posible implementación de la clase Pila

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -11,12 +11,42 @@ namespace Torres_de_Hanoi
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b)
         {
-
+            if(a.Top < b.Top)
+            {
+                b.push(a.pop());
+            }
+            if(b.Top < a.Top)
+            {
+                a.push(b.pop());
+            }
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int m = 0;
+
+            if(n%2 != 0)
+            {
+                while(fin.Size != n)
+                {
+                    m++;
+                    mover_disco(ini, fin);
+                    mover_disco(ini, aux);
+                    mover_disco(aux, fin);
+                }
+            } 
+            else
+            {
+                while (fin.Size != n)
+                {
+                    m++;
+                    mover_disco(ini, aux);
+                    mover_disco(ini, fin);
+                    mover_disco(aux, fin);
+                }
+            }
+
+            return m;
         }
 
     }
